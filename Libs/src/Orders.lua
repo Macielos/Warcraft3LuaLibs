8b9e2da47ca0f2e22004ac3b040615b7b262aa88
Macielos <maciek.ganko@gmail.com>
2025-07-19 00:49:20 +0200

    added Orders file

---
+++ Libs/src/Orders.lua
@@ -0,0 +1,10 @@
+if Debug then Debug.beginFile "Orders" end
+do
+    ORDER_ID_ATTACK = 851983        --attack, attack-move
+    ORDER_ID_ATTACK_GROUND = 851984
+    ORDER_ID_ATTACK_ONCE = 851985
+    ORDER_ID_MOVE = 851986
+    ORDER_ID_PATROL = 851990
+    ORDER_ID_SMART = 851971         --right click/move, right-click/attack
+end
+if Debug then Debug.endFile() end
